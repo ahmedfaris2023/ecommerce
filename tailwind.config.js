02e8b37c695a211bdeaff3802a0b730d42217550
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-11-14 17:13:39 +0300

    home screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: "#08D9D6",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
